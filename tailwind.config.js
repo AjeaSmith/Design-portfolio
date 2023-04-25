d4adbb49e1f444c754d6578eb65fbf5b800b3dc2
ajea <smithajea@rocketmail.com>
2023-04-25 16:19:54 -0400

    added design system to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 module.exports = {
  content: ["./*.html"],
   theme: {
-    extend: {},
+    fontFamily: {
+      'body': ['Plus Jakarta Sans']
+    },
+    fontSize: {
+      sm: ['32px', '40px'],
+      md: ['40px', '51px'],
+      lg: ['56px', '70px'],
+    },
+    extend: {
+      colors:{
+        purple: '#755CDE',
+        darkOrange: '#EB7565',
+        lightOrange: '#F6A560',
+        pink: '#F39E9E',
+        teal: '#61C4B7',
+        violet: "#552049",
+        black: "#030303",
+        gray: "#7A746E",
+        cream: "#FFF7F0"
+      },
+    },
   },
   plugins: [],
 }
